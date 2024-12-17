a77198369bae9a632155e649f7ac0131ed010432
Myo Thiha <myothiha4112004@gmail.com>
2024-12-17 13:12:40 +0630

    first commit

---
+++ src/app/posts/all-post/all-post.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { PostsService } from '../../services/posts.service';
+
+@Component({
+  selector: 'app-all-post',
+  templateUrl: './all-post.component.html',
+  styleUrl: './all-post.component.css'
+})
+export class AllPostComponent implements OnInit {
+
+  postArray!: Array<any>;
+
+  constructor(private postsService: PostsService) {}
+
+  ngOnInit(): void {
+      this.postsService.loadData().subscribe(val => {
+        this.postArray = val;
+      })
+  }
+
+  onDelete(postImgPath: any, id: any) {
+    if (confirm('Are you sure you want to delete this post?')) {
+      this.postsService.deleteImage(postImgPath, id);
+    }
+    
+  }
+}
